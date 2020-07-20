6bfaf7df2850736943057b8982f0635f136adaa2
dirkschub <dirkschub@gmx.de>
2020-07-20 13:53:48 +0200

    data

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(de_DE\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+usermod -s /usr/bin/zsh root
+cp -aT /etc/skel/ /root/
+chmod 700 /root
+
+# add liveuser
+	useradd -m liveuser -u 500 -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash
+	chown -R liveuser:users /home/liveuser
+	passwd -d liveuser
+	# enable autologin
+	groupadd -r autologin
+	gpasswd -a liveuser autologin
+	groupadd -r nopasswdlogin
+	gpasswd -a liveuser nopasswdlogin
+	echo "The account liveuser with no password has been created"
+
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+systemctl enable pacman-init.service choose-mirror.service
+systemctl set-default multi-user.target
+systemctl enable lightdm.service
+systemctl set-default graphical.target
