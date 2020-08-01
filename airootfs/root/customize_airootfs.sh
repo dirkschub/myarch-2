6470156e1aefe0e07fccb870bad29e10a3ea7b54
dirkschub <dirkschub@gmx.de>
2020-08-01 21:46:52 +0200

    update

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 set -e -u
+
+echo 'Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.'
 
 sed -i 's/#\(de_DE\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
